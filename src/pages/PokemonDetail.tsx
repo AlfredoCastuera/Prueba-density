736de486834827a1a21ff7862eca8a598454a6b1
Freddy castuera <freddycastuera@gmail.com>
2023-02-09 23:09:09 -0700

    Add colored badges to pokemon detail page

--- src/pages/PokemonDetail.tsx
+++ src/pages/PokemonDetail.tsx
@@ -3,7 +3,7 @@ import { useAppDispatch, useAppSelector } from '../Redux/hooks'
 import { fetchPokemon, selectPokemon, selectNext, selectPrevious } from '../Redux/features/pokemonSlice'
 import { fetchPokemonByURL, selectCurrentPokemon } from '../Redux/features/currentPokemonSlice'
 import  BarChart  from '../components/BarChart'
-import { Container, Typography, Avatar, Paper } from '@mui/material'
+import { Container, Typography, Avatar, Paper, Chip } from '@mui/material'
 import { useTheme } from '@mui/material/styles';
 import classes from './PokemonDetail.module.scss'
 
@@ -13,15 +13,22 @@ const PokemonDetail = () => {
   console.log(currentPokemon);
 
   const stats = currentPokemon.stats.map((stat:any)=>stat.base_stat)
-  console.log('los stats son ', stats);
+  const types= currentPokemon.types.map((type:any)=>type.type.name);
+
+  function generateRandomColorHex() {
+    return "#" + ("00000" + Math.floor(Math.random() * Math.pow(16, 6)).toString(16)).slice(-6);
+  }
   return (
     <Container maxWidth="md">
       <Paper elevation={3} className={classes.pokemonDetail_card}>
         <Avatar src={currentPokemon.sprites.front_default}  sx={{ width: 64, height: 64, bgcolor:theme.palette.primary.light }} />
         <Typography variant='h5' color={theme.palette.primary.main}>{currentPokemon.name}</ Typography>
-        <Typography variant='h5' color={theme.palette.primary.main}>{currentPokemon.order}</ Typography>
-        <Typography variant='h5' color={theme.palette.primary.main}>{currentPokemon.height}</ Typography>
-        <Typography variant='h5' color={theme.palette.primary.main}>{currentPokemon.weight}</ Typography>
+        {Array.isArray(types) && types.map((type)=>{
+          return (<Chip label={type} sx={{ bgcolor: generateRandomColorHex(), color: theme.palette.grey[100]}}/>)
+        })}
+        <Typography variant='body1' color={theme.palette.primary.main}>Number: {currentPokemon.order}</ Typography>
+        <Typography variant='body1' color={theme.palette.primary.main}>Height: {currentPokemon.height}</ Typography>
+        <Typography variant='body1' color={theme.palette.primary.main}>Weight: {currentPokemon.weight}</ Typography>
       </ Paper>
       <BarChart color={theme.palette.primary.light} borderColor={theme.palette.primary.dark} stats={stats}/>
     </Container>
